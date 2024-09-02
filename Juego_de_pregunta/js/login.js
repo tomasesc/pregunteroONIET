b49c0c3243af024718c5d191dd3143a187b2dfa4
Odouglas2003 <octaviodouglas200@gmail.com>
2024-09-02 16:20:57 -0300

    agregando el frontend

---
+++ Juego_de_pregunta/js/login.js
@@ -0,0 +1,15 @@
+
+        document.getElementById('loginForm').addEventListener('submit', function(event) {
+            event.preventDefault(); // Evita que el formulario se envíe
+            let dni = document.getElementById('dni').value;
+            let contaseña = document.getElementById('password').value;
+            let email = document.getElementById('email').value;
+
+            // Validar que el email sea de Gmail o Hotmail
+            if (email.endsWith("@gmail.com") || email.endsWith("@hotmail.com") || email.endsWith("@yahoo.com")){
+                alert("¡Bienvenido, " + dni + " " + contaseña + "!");
+                location.href= "./page/preguntado.html"
+            } else {
+                alert("Por favor, ingrese un correo de Gmail o Hotmail válido.");
+            }
+        });
